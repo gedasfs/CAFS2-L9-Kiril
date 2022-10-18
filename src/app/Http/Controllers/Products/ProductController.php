ee06db6c2487204f0483e2310f70f46d090a5c89
Kiril <hello@nonamez.name>
2022-10-18 19:44:14 +0300

    Added: query builder

--- src/app/Http/Controllers/Products/ProductController.php
+++ src/app/Http/Controllers/Products/ProductController.php
@@ -5,16 +5,38 @@ namespace App\Http\Controllers\Products;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::get();
+        // $products = DB::select('select * from products where category_id < 10 and is_active = 1 order by id desc limit 3');
 
-        return view('products.index', [
-            'products' => $products
-        ]);
+        $productsQuery = DB::table('products');
+
+        if ($request->has('category_id')) {
+            $productsQuery->where('category_id', $request->get('category_id'));
+        }
+        
+        if ($request->has('active')) {
+            $productsQuery->where('is_active', true);
+        }
+
+        if ($request->has('limit')) {
+            // $productsQuery->limit($request->get('limit'));
+            $productsQuery->take($request->get('limit'));
+        }
+
+        $productsQuery->orderBy('id', 'desc');
+
+        // $productsQuery->select('id', 'name');
+
+        $products = $productsQuery->get();
+
+        // $products = Product::get();
+
+        return view('products.index', compact('products'));
     }
     
     public function create()
@@ -32,3 +54,4 @@ class ProductController extends Controller
         return view('products.show');
     }
 }
+
